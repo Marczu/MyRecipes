fc0f57519af57af4f720dcf3ee580dd0b4c415e7
Marczu <marczu09@gmail.com>
2018-11-10 21:26:35 +0100

    Add recipes to db

--- src/main/java/com/mejner/recipeapp/repositories/RecipeRepository.java
+++ src/main/java/com/mejner/recipeapp/repositories/RecipeRepository.java
@@ -3,5 +3,5 @@ package com.mejner.recipeapp.repositories;
 import com.mejner.recipeapp.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RecipieRepository extends CrudRepository<Recipe, Long> {
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 }
