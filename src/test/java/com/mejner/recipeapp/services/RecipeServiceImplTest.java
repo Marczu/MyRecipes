7d20afaa0c19ab69a52f059a99313c80add43804
Marcin Mejner <marczu09@gmail.com>
2018-11-16 10:20:42 +0100

    Add Tests

---
+++ src/test/java/com/mejner/recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,73 @@
+package com.mejner.recipeapp.services;
+
+
+import com.mejner.recipeapp.domain.Recipe;
+import com.mejner.recipeapp.repositories.RecipeRepository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by jt on 6/17/17.
+ */
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipesById() throws Exception {
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1l);
+
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+
+        verify(recipeRepository, times(1)).findById(anyLong());
+
+
+
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+
+        Recipe recipe = new Recipe();
+        HashSet receipesData = new HashSet();
+        receipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(receipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+
+}
