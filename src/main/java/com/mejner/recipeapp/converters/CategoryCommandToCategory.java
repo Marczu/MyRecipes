127ecd4f2631568b7e509ca45a2c1d0e92afa7d6
Marcin Mejner <marczu09@gmail.com>
2018-11-19 13:22:55 +0100

    Add Commands and concerters

---
+++ src/main/java/com/mejner/recipeapp/converters/CategoryCommandToCategory.java
@@ -0,0 +1,29 @@
+package com.mejner.recipeapp.converters;
+
+
+import com.mejner.recipeapp.commands.CategoryCommand;
+
+import com.mejner.recipeapp.domain.Category;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CategoryCommandToCategory implements Converter<CategoryCommand, Category>{
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Category convert(CategoryCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final Category category = new Category();
+        category.setId(source.getId());
+        category.setDescription(source.getDescription());
+        return category;
+    }
+}
---
+++ src/main/java/com/mejner/recipeapp/converters/UnitOfMeasureToUnitOfMeasureCommand.java
@@ -0,0 +1,28 @@
+package com.mejner.recipeapp.converters;
+
+import com.mejner.recipeapp.commands.UnitOfMeasureCommand;
+import com.mejner.recipeapp.domain.UnitOfMeasure;
+
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class UnitOfMeasureToUnitOfMeasureCommand implements Converter<UnitOfMeasure, UnitOfMeasureCommand> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasureCommand convert(UnitOfMeasure unitOfMeasure) {
+
+        if (unitOfMeasure != null) {
+            final UnitOfMeasureCommand uomc = new UnitOfMeasureCommand();
+            uomc.setId(unitOfMeasure.getId());
+            uomc.setDescription(unitOfMeasure.getDescription());
+            return uomc;
+        }
+        return null;
+    }
+}
